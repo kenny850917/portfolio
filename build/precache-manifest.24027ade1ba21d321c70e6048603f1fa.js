54082bb39b1390c751e125771045d89718cacbd7
kenny850917 <35354115+kenny850917@users.noreply.github.com>
2020-10-02 21:07:44 +0800

    run build

---
+++ build/precache-manifest.24027ade1ba21d321c70e6048603f1fa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb63741a8e4ee1055295d170a33f5ad",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e8869d5787e4c20018d1",
+    "url": "/static/css/2.af621169.chunk.css"
+  },
+  {
+    "revision": "77d0e540f85a444cf4f2",
+    "url": "/static/css/main.14b7d81f.chunk.css"
+  },
+  {
+    "revision": "e8869d5787e4c20018d1",
+    "url": "/static/js/2.4ed5aefd.chunk.js"
+  },
+  {
+    "revision": "77d0e540f85a444cf4f2",
+    "url": "/static/js/main.c8986446.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
